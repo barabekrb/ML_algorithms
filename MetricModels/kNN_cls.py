7f2b0db3eb2b908b49c24b7a83310708eff7245f
barabekrb <emilfedorov2001@gmail.com>
2024-04-27 19:07:05 +0300

    Add predict and predict_proba methods

--- MetricModels/kNN_cls.py
+++ MetricModels/kNN_cls.py
@@ -21,3 +21,27 @@ class kNN_cls():
         self.X = X_.copy()
         self.y = y_.copy()
         self.train_size = self.X.shape
+
+    
+    def predict(self, X_):
+        y_s = []
+        for ind, row in X_.iterrows():
+            cls_pred = []
+            for j, rs in self.X.iterrows():
+                cls_pred.append([j, np.linalg.norm(row - rs)])
+            cls_pred = pd.DataFrame(cls_pred, columns=['ind', 'norm'])
+            cls_pred.sort_values(by=["norm"], ascending=True, inplace=True, ignore_index=True)
+            y_s.append(self.y[cls_pred[:self.k]['ind']].mode().sort_values(ascending=False, ignore_index=True)[0])
+        return np.array(y_s)
+
+    
+    def predict_proba(self, X_):
+        y_s = []
+        for ind, row in X_.iterrows():
+            cls_pred = []
+            for j, rs in self.X.iterrows():
+                cls_pred.append([j, np.linalg.norm(row - rs)])
+            cls_pred = pd.DataFrame(cls_pred, columns=['ind', 'norm'])
+            cls_pred.sort_values(by=["norm"], ascending=True, inplace=True, ignore_index=True)
+            y_s.append(self.y[cls_pred[:self.k]['ind']].sum() / self.k)
+        return np.array(y_s)
